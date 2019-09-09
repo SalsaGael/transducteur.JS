820cec65a3c72d274680968b068e56251237ea7c
Gael Piconcely <gael.piconcely@free.fr>
2019-09-09 20:02:42 +0200

    InstallPWA fix

--- src/js/installPWA.js
+++ src/js/installPWA.js
@@ -11,38 +11,38 @@ const installPWA = () => {
       <a> Installer la PWA</a>
     `;
     menulist.append(li);
+
+    let deferredPrompt = null;
+
+    window.addEventListener("beforeinstallprompt", e => {
+      // Prevent Chrome 67 and earlier from automatically showing the prompt
+      e.preventDefault();
+      // Stash the event so it can be triggered later.
+      deferredPrompt = e;
+    });
+
+    const install = async () => {
+      if (deferredPrompt) {
+        deferredPrompt.prompt();
+        console.log(deferredPrompt);
+        deferredPrompt.userChoice.then(function(choiceResult) {
+          if (choiceResult.outcome === "accepted") {
+            console.log("Your PWA has been installed");
+          } else {
+            console.log("User chose to not install your PWA");
+          }
+          deferredPrompt = null;
+        });
+      }
+    };
+
+    const installButton = document.querySelector("#installButton");
+
+    installButton.onclick = e => {
+      e.preventDefault();
+      install();
+    };
   }
-
-  let deferredPrompt = null;
-
-  window.addEventListener("beforeinstallprompt", e => {
-    // Prevent Chrome 67 and earlier from automatically showing the prompt
-    e.preventDefault();
-    // Stash the event so it can be triggered later.
-    deferredPrompt = e;
-  });
-
-  const install = async () => {
-    if (deferredPrompt) {
-      deferredPrompt.prompt();
-      console.log(deferredPrompt);
-      deferredPrompt.userChoice.then(function(choiceResult) {
-        if (choiceResult.outcome === "accepted") {
-          console.log("Your PWA has been installed");
-        } else {
-          console.log("User chose to not install your PWA");
-        }
-        deferredPrompt = null;
-      });
-    }
-  };
-
-  const installButton = document.querySelector("#installButton");
-
-  installButton.onclick = e => {
-    e.preventDefault();
-    install();
-  };
 };
 
 export default installPWA;
